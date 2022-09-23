42556b6bb30a340f70e75a14695700808d03cb7a
Matt Taylor <taylor.matt777@gmail.com>
2022-09-22 21:31:15 -0600

    Group bds by day + infinite scroll, geo fail case

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -16,11 +16,13 @@ interface Beatdown {
   address: string;
   lat: number;
   long: number;
+  milesFromMe: number;
 }
 
 interface Day {
-  dateDisplay: string;
-  beatdowns: Beatdown[];
+  daysFromToday: number;  // 0 for today, 1 tomorrow, etc
+  dateDisplay: string;    // Today, Tomorrow, Wednesday, Monday July 12, etc
+  beatdowns: Beatdown[];  // the beatdowns ordered by distance from you
 }
 
 interface Coords {
@@ -33,7 +35,11 @@ const BOISE_COORDS: Coords = {
   longitude: -116.2023,
 };
 
-const MAX_MILES = 25;
+const MAX_MILES = 30;
+
+const DAYS = [
+  'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'
+];
 
 @Component({
   selector: 'app-tab1',
@@ -42,19 +48,26 @@ const MAX_MILES = 25;
 })
 export class Tab1Page {
   allBDs: Beatdown[];
-  nearby: Beatdown[];
+  nearbyMap = new Map<string, Beatdown[]>;
+  days: Day[];
+  limit = MAX_MILES;
+
   myLocation: Coords;
+  locationFailure = false;
 
   constructor(
       private readonly http: HttpService,
   ) {}
 
   ngOnInit() {
+    this.loadFromCache();
     this.loadBeatdowns();
   }
 
+  /**
+   * Load the beatdowns from the API
+   */
   async loadBeatdowns() {
-    this.loadFromCache();
     const unparsed = await this.http.get(URL);
 
     this.allBDs = unparsed.values.map((bd, index) => {
@@ -97,6 +110,9 @@ export class Tab1Page {
     this.setMyLocation();
   }
 
+  /**
+   * Try to get the user's location, but fallback to Boise's coordinates for now
+   */
   setMyLocation() {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
@@ -111,6 +127,7 @@ export class Tab1Page {
    * Handle successfully getting the user's location
    */
   onGeolocationSuccess(position: GeolocationPosition) {
+    console.log(position);
     const {latitude, longitude} = position.coords;
     this.myLocation = {latitude, longitude};
     this.setNearbyBeatdowns();
@@ -120,24 +137,78 @@ export class Tab1Page {
    * Handle being unable to load the user's location
    */
   onGeolocationFailure(failure?: GeolocationPositionError) {
-    this.myLocation = BOISE_COORDS;
     console.log(failure);
+    this.locationFailure = true;
+    this.myLocation = BOISE_COORDS;
     this.setNearbyBeatdowns();
   }
 
   /**
-   * Filter down to the nearby beatdowns
+   * Filter down to the nearby beatdowns and set the data in the app
+   * This will act as a reset as well
    */
   setNearbyBeatdowns() {
-    this.nearby = this.allBDs.filter(bd => {
+    this.nearbyMap = new Map<string, Beatdown[]>();
+    this.days = [];
+
+    const nearby = this.allBDs.filter(bd => {
       const dist = this.distance(
           bd.lat,
           bd.long,
           this.myLocation.latitude,
           this.myLocation.longitude,
       );
+
+      // side effect, but idc, we need the milesFromMe
+      bd.milesFromMe = dist;
+
       return dist < MAX_MILES;
     });
+
+    // sort the near
+    nearby.sort((a, b) => a.milesFromMe - b.milesFromMe);
+    nearby.forEach(bd => {
+      const bds = this.nearbyMap.get(bd.dayOfWeek) ?? [];
+      bds.push(bd);
+      this.nearbyMap.set(bd.dayOfWeek, bds);
+    });
+
+    // now that we have the sorted filtered list we care about, build out
+    // the schedule of days
+    this.loadOneWeek();
+  }
+
+  /**
+   * Load in 7 days at a time, can infinite scroll
+   */
+  loadOneWeek($event?: any) {
+    // handle this being an ionic scroll event
+    const ionicScrollEvent = $event as {target: {complete: Function}};
+
+    // determine the day to start on
+    const today = new Date();
+    const todayDay = today.getDay();  // 4 = Wednesday
+    let daysFromToday =
+        this.days?.length ? this.days[this.days.length - 1].daysFromToday : 0;
+
+    // if we're past noon, don't show today's BDs any longer
+    if (today.getHours() >= 12) daysFromToday++;
+
+    // load in the next 7 days
+    for (let i = daysFromToday; i < daysFromToday + 7; i++) {
+      // add on todayDay to ensure we're picking the correct day to display
+      const dayOfWeek = DAYS[(todayDay + i) % 7];
+      const beatdowns = this.nearbyMap.get(dayOfWeek) ?? [];
+
+      // only build up days that have beatdowns
+      if (beatdowns.length > 0) {
+        const dateDisplay = this.getDateDisplay(today, i);
+        this.days.push({daysFromToday: i, dateDisplay, beatdowns});
+      }
+    }
+
+    // if this was an ionic scroll event, complete it
+    if (ionicScrollEvent) ionicScrollEvent.target.complete();
   }
 
   /**
@@ -161,6 +232,25 @@ export class Tab1Page {
       dist = dist * 60 * 1.1515;
       return dist;
     }
+  }
+
+  /**
+   * Return a pretty string to represent the day offset from today
+   */
+  getDateDisplay(today: Date, offset: number) {
+    if (offset === 0) return 'Today';
+    if (offset === 1) return 'Tomorrow';
+    if (offset < 7) return DAYS[(today.getDay() + offset) % 7];
+    return this.addDays(today, offset).toDateString();
+  }
+
+  /**
+   * Return a new date that is "days" days later
+   */
+  addDays(date: Date, days: number): Date {
+    var result = new Date(date);
+    result.setDate(result.getDate() + days);
+    return result;
   }
 
   /**
